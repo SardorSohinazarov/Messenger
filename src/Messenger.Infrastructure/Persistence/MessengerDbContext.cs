e68e9b96a99db79f41036adaf37f6f8d84788077
SardorSohinazarov <sardorstudent0618@gmail.com>
2024-10-07 14:45:40 +0500

    DbContext va User DbSet

---
+++ src/Messenger.Infrastructure/Persistence/MessengerDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Messenger.Domain.Entities;
+
+namespace Messenger.Infrastructure.Persistence
+{
+    public class MessengerDbContext : DbContext
+    {
+        //user va unga aloqador setlar
+        public DbSet<User> Users { get; set; }
+
+        //constructorlar
+        public MessengerDbContext() { }
+
+        public MessengerDbContext(DbContextOptions<MessengerDbContext> options) : base(options) 
+            => Database.Migrate(); // Migratsiyani avtomatik ishga tushirish
+    }
+}
