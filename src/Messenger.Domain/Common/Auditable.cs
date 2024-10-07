d6e183af53b5fce1b7b64ca8c21819cafe37d0d3
SardorSohinazarov <sardorstudent0618@gmail.com>
2024-10-07 23:48:57 +0500

    Some changes

--- src/Messenger.Domain/Common/Auditable.cs
+++ src/Messenger.Domain/Common/Auditable.cs
@@ -3,7 +3,7 @@
     public abstract class Auditable<TId> : BaseEntity<TId>
     {
         public string CreatedBy { get; set; }
-        public DateTime CreatedAt { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
         public string LastModifiedBy { get; set; }
         public DateTime? LastModifiedAt { get; set; }
     }
--- src/Messenger.Domain/Entities/User.cs
+++ src/Messenger.Domain/Entities/User.cs
@@ -6,7 +6,7 @@ namespace Messenger.Domain.Entities
     {
         public string FirstName { get; set; }  // Foydalanuvchi ismi
         public string LastName { get; set; }  // Ixtiyoriy. Foydalanuvchi familiyasi
-        public string Username { get; set; }  // Ixtiyoriy. Foydalanuvchi foydalanuvchi nomi
+        public string UserName { get; set; }  // Ixtiyoriy. Foydalanuvchi foydalanuvchi nomi
         public string PhoneNumber { get; set; }  // Foydalanuvchi telefon raqami
         public string Email { get; set; }  // Ixtiyoriy. Foydalanuvchi elektron pochta manzili
         public string LanguageCode { get; set; }  // Ixtiyoriy. Foydalanuvchining til kodi (IETF)
