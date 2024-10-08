c41b62de0a116b306492e8953a3690c6d71cc7d5
SardorSohinazarov <sardorstudent0618@gmail.com>
2024-10-09 00:22:18 +0500

    Email Service

---
+++ src/Messenger.Application/Services/Email/EmailService.cs
@@ -0,0 +1,45 @@
+﻿using Messenger.Application.DataTransferObjects;
+using Microsoft.Extensions.Options;
+using System.Net;
+using System.Net.Mail;
+
+namespace Messenger.Application.Services.Email
+{
+    public class EmailService : IEmailService
+    {
+        private readonly SmtpSettings _smtpSettings;
+
+        public EmailService(IOptions<SmtpSettings> smtpSettings) 
+            => _smtpSettings = smtpSettings.Value;
+
+        public async Task SendEmailAsync(string to, string toDisplayName, string subject, string body)
+        {
+            using (var client = new SmtpClient())
+            {
+                client.Host = _smtpSettings.Host;// "smtp.gmail.com";
+                client.Port = _smtpSettings.Port;// 587;
+                client.DeliveryMethod = SmtpDeliveryMethod.Network;
+                client.UseDefaultCredentials = false;
+                client.EnableSsl = _smtpSettings.EnableSsl;// true;
+                client.Credentials = new NetworkCredential(_smtpSettings.Username, _smtpSettings.Password);
+
+                using (var message = new MailMessage(
+                    from: new MailAddress(
+                        address: _smtpSettings.Username,
+                        displayName: _smtpSettings.DisplayName),
+                    to: new MailAddress(
+                        address: to,
+                        displayName: toDisplayName)
+                    ))
+                {
+
+                    message.Subject = subject;
+                    message.Body = body;
+                    message.IsBodyHtml = true;
+
+                    await client.SendMailAsync(message);
+                }
+            }
+        }
+    }
+}
---
+++ src/Messenger.Application/Services/Email/IEmailService.cs
@@ -0,0 +1,7 @@
+﻿namespace Messenger.Application.Services.Email
+{
+    public interface IEmailService
+    {
+        Task SendEmailAsync(string to, string toDisplayName, string subject, string body);
+    }
+}
