365942b2ff71ce6912fcadb95c5e2644f2f263ac
SardorSohinazarov <sardorstudent0618@gmail.com>
2024-10-09 00:22:43 +0500

    Modellar almashdi migration

--- src/Messenger.Domain/Common/Auditable.cs
+++ src/Messenger.Domain/Common/Auditable.cs
@@ -2,9 +2,9 @@
 {
     public abstract class Auditable<TId> : BaseEntity<TId>
     {
-        public string CreatedBy { get; set; }
-        public DateTime CreatedAt { get; set; } = DateTime.Now;
-        public string LastModifiedBy { get; set; }
+        public long CreatedBy { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public long LastModifiedBy { get; set; }
         public DateTime? LastModifiedAt { get; set; }
     }
 }
--- src/Messenger.Domain/Entities/User.cs
+++ src/Messenger.Domain/Entities/User.cs
@@ -5,11 +5,11 @@ namespace Messenger.Domain.Entities
     public class User : Auditable<long>
     {
         public string FirstName { get; set; }  // Foydalanuvchi ismi
-        public string LastName { get; set; }  // Ixtiyoriy. Foydalanuvchi familiyasi
-        public string UserName { get; set; }  // Ixtiyoriy. Foydalanuvchi foydalanuvchi nomi
+        public string? LastName { get; set; }  // Ixtiyoriy. Foydalanuvchi familiyasi
+        public string? UserName { get; set; }  // Ixtiyoriy. Foydalanuvchi foydalanuvchi nomi
         public string PhoneNumber { get; set; }  // Foydalanuvchi telefon raqami
         public string Email { get; set; }  // Ixtiyoriy. Foydalanuvchi elektron pochta manzili
-        public string LanguageCode { get; set; }  // Ixtiyoriy. Foydalanuvchining til kodi (IETF)
+        public string? LanguageCode { get; set; }  // Ixtiyoriy. Foydalanuvchining til kodi (IETF)
 
         public string PasswordHash { get; set; }  // Foydalanuvchi parolining hash qiymati
         public string Salt { get; set; }  // Parolni himoyalash uchun ishlatiladigan tuz
