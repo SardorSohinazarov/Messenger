fa89015c142a6b190a68eba2cb31351ed974d07a
SardorSohinazarov <sardorstudent0618@gmail.com>
2024-10-13 03:38:48 +0500

    Excetions

---
+++ src/Messenger.Domain/Exceptions/ValidationException.cs
@@ -0,0 +1,26 @@
+﻿using System.Runtime.Serialization;
+
+namespace Messenger.Domain.Exceptions
+{
+    public class ValidationException : Exception
+    {
+        public ValidationException() : base("Model is invalid.")
+        {
+        }
+
+        public ValidationException(string message)
+            : base(message)
+        {
+        }
+
+        public ValidationException(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+
+        protected ValidationException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        {
+        }
+    }
+}
