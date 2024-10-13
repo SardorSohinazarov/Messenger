3248b4b10ed3665ae08f685809b9d08a868c43f3
SardorSohinazarov <sardorstudent0618@gmail.com>
2024-10-13 05:58:25 +0500

    Entitiy o'zgarib migration

--- src/Messenger.Domain/Entities/LinkTables.cs
+++ src/Messenger.Domain/Entities/LinkTables.cs
@@ -4,13 +4,14 @@ namespace Messenger.Domain.Entities
 {
     public class ChatUser : Auditable<Guid>, ISoftDeletable
     {
-        public bool IsBlocked { get; set; }
-
         public long UserId { get; set; }
         public long ChatId { get; set; }
 
+        public bool IsBlocked { get; set; }
+        public bool IsAdmin { get; set; }
+        public bool IsDeleted { get; set; }
+
         public User User { get; set; }
         public Chat Chat { get; set; }
-        public bool IsDeleted { get; set; }
     }
 }
