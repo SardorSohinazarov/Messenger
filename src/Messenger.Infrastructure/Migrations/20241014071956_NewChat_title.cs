32d9dfaaa898284baa11c011347f611ff05cc782
SardorSohinazarov <sardorstudent0618@gmail.com>
2024-10-14 23:18:17 +0500

    Migration

---
+++ src/Messenger.Infrastructure/Migrations/20241014071956_NewChat_title.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Messenger.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class NewChat_title : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Messages_Users_NewChatMemberId",
+                table: "Messages");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "NewChatTitle",
+                table: "Messages",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<long>(
+                name: "NewChatMemberId",
+                table: "Messages",
+                type: "bigint",
+                nullable: true,
+                oldClrType: typeof(long),
+                oldType: "bigint");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Messages_Users_NewChatMemberId",
+                table: "Messages",
+                column: "NewChatMemberId",
+                principalTable: "Users",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Messages_Users_NewChatMemberId",
+                table: "Messages");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "NewChatTitle",
+                table: "Messages",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<long>(
+                name: "NewChatMemberId",
+                table: "Messages",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L,
+                oldClrType: typeof(long),
+                oldType: "bigint",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Messages_Users_NewChatMemberId",
+                table: "Messages",
+                column: "NewChatMemberId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
