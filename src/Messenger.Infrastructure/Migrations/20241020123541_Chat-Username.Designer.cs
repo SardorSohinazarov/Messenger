48b50430e1480c44e4a333c0a9b546be429e51e0
SardorSohinazarov <sardorstudent0618@gmail.com>
2024-10-20 17:39:55 +0500

    Migration yaratdim

--- src/Messenger.Infrastructure/Migrations/20241020123541_Chat-Username.Designer.cs
+++ src/Messenger.Infrastructure/Migrations/20241020123541_Chat-Username.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Messenger.Infrastructure.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Messenger.Infrastructure.Migrations
 {
     [DbContext(typeof(MessengerDbContext))]
-    partial class MessengerDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241020123541_Chat-Username")]
+    partial class ChatUsername
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/Messenger.Infrastructure/Migrations/20241020123541_Chat-Username.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Messenger.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChatUsername : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Username",
+                table: "Chats",
+                newName: "UserName");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "UserName",
+                table: "Chats",
+                newName: "Username");
+        }
+    }
+}
